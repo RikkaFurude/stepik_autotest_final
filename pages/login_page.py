c1f3e7ab97363f55f8179e87b9250c6d2bf8c6ca
RikkaFurude <94025617+RikkaFurude@users.noreply.github.com>
2026-02-25 14:14:27 +0500

    Update login_page.py

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,21 +1,24 @@
 from .base_page import BasePage
 from .locators import LoginPageLocators
 
+
 class LoginPage(BasePage):
     def should_be_login_page(self):
         self.should_be_login_url()
         self.should_be_login_form()
         self.should_be_register_form()
 
-    def should_be_login_url(self):
-        # Проверяем, что подстрока "login" есть в текущем URL
-        current_url = self.browser.current_url
-        assert "login" in current_url, f"Expected 'login' in URL, but got {current_url}"
+    def should_be_login_url(self):  # проверка на корректный url адрес
+        assert "login" in self.browser.current_url, "Incorrect login URL"
 
-    def should_be_login_form(self):
-        # Проверка наличия формы логина
-        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
+    def should_be_login_form(self):  # проверка, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is absent"
 
-    def should_be_register_form(self):
-        # Проверка наличия формы регистрации
-        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
+    def should_be_register_form(self):  # проверка, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is absent"
+
+    def register_new_user(self, email, password):  # регистрация нового пользователя
+        self.browser.find_element(*LoginPageLocators.REGISTER_EMAIL).send_keys(email)
+        self.browser.find_element(*LoginPageLocators.REGISTER_PASSWORD).send_keys(password)
+        self.browser.find_element(*LoginPageLocators.REGISTER_PASSWORD_REPEAT).send_keys(password)
+        self.browser.find_element(*LoginPageLocators.REGISTER_BUTTON).click()
