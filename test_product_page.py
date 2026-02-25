74f6f3065c6c045da7d68a9db615143fcb8433cc
RikkaFurude <longin.2000@mail.ru>
2026-02-25 14:01:26 +0500

    Move login link methods to BasePage and add tests for product page

--- test_product_page.py
+++ test_product_page.py
@@ -1,8 +1,19 @@
 import pytest
-from pages.product_page import ProductPage
+from .pages.product_page import ProductPage
 
-def test_guest_should_see_login_link_on_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-    page = ProductPage(browser, link)
-    page.open()
-    page.should_be_login_link()
+@pytest.mark.login_guest
+class TestLoginFromProductPage():
+    # Фикстура setup: подготовит ссылку перед каждым тестом
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self):
+        self.link = "http://selenium1py.pythonanywhere.com"
+    
+    def test_guest_should_see_login_link_on_product_page(self, browser):
+        page = ProductPage(browser, self.link)
+        page.open()
+        page.should_be_login_link()
+
+    def test_guest_can_go_to_login_page_from_product_page(self, browser):
+        page = ProductPage(browser, self.link)
+        page.open()
+        page.go_to_login_page()
